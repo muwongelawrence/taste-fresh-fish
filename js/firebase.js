b87b5a6c16ec8ff0de56cfdd34c428a38491ef66
muwongelawrence <muwongelawrence@cis.mak.ac.ug>
2021-02-23 10:48:20 -0800

    small change

--- js/firebase.js
+++ js/firebase.js
@@ -4,17 +4,31 @@
   var fireBase = fireBase || firebase;
   var hasInit = false;
   var firebaseConfig = {
-    apiKey: "AIzaSyCiRmoaUdI2bDajDdCAIqw4lv6Ajg5BzlQ",
-    authDomain: "fresh-fish-8ab3b.firebaseapp.com",
-    projectId: "fresh-fish-8ab3b",
-    storageBucket: "fresh-fish-8ab3b.appspot.com",
-    messagingSenderId: "962868395749",
-    appId: "1:962868395749:web:b347d8f4d34d491609de0c",
-    measurementId: "G-WYKYYX3R4M"
+    apiKey: "AIzaSyAh3kjAEoJPPx2mH3atrbG51VqvBJJv130",
+    authDomain: "fresh-fish-species.firebaseapp.com",
+    projectId: "fresh-fish-species",
+    storageBucket: "fresh-fish-species.appspot.com",
+    messagingSenderId: "474045900052",
+    appId: "1:474045900052:web:102a1ca75b9f8900e00e01",
+    measurementId: "G-ZW86HTT4KY"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+
+
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyCiRmoaUdI2bDajDdCAIqw4lv6Ajg5BzlQ",
+  //   authDomain: "fresh-fish-8ab3b.firebaseapp.com",
+  //   projectId: "fresh-fish-8ab3b",
+  //   storageBucket: "fresh-fish-8ab3b.appspot.com",
+  //   messagingSenderId: "962868395749",
+  //   appId: "1:962868395749:web:b347d8f4d34d491609de0c",
+  //   measurementId: "G-WYKYYX3R4M"
+  // };
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
 
   // var firebaseConfig = {
   //   apiKey: "AIzaSyAmEZMciOXaGaUju3NdXmR6yVoBU2t9fNc",
